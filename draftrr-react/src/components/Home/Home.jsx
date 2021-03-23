51fbc120eb02782dfc9b8f21dc354dc5f54ccd24
nblack0917 <nickablack@gmail.com>
2021-03-23 15:57:14 -0500

    added svg images

--- draftrr-react/src/components/Home/Home.jsx
+++ draftrr-react/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import "./Home.scss"
+import HomeImage from '../../img/HomeImage'
 
 export const Home = () => {
     return (
         <div className="body-container">
             Home
+            <HomeImage width="500" />
         </div>
     )
 }
