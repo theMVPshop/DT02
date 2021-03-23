79bea9d075e3a1698091ebf4bfb856e3f97a4fd4
Lane Garner <b.lane.garner@gmail.com>
2021-03-23 16:54:30 -0500

    logo in header

--- draftrr-react/src/components/Header/Header.jsx
+++ draftrr-react/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom"
 
 import Login from "../Login"
+import { Logo } from "../../img/Logo"
+import DraftrrLogo from "../../img/DraftrrLogo.jsx";
+
 import { Nav } from "react-bootstrap"
 
 import "./Header.scss"
@@ -8,21 +11,24 @@ import "./Header.scss"
 export const Header = () => {
     return (
         <header className="pt-2 d-flex justify-content-end">
-            <Link className="container pl-3" to="/">
-                <h1>Draftrr</h1>
+            <Link className="d-flex align-items-center container pl-3 pt-1" to="/">
+                <div className="mr-3">
+                <Logo width="4em" />
+                </div>
+                <DraftrrLogo width="7em" />
             </Link>
             <Nav
                 className="d-flex justify-content-end"
                 activeKey="/home"
             >
-            <Nav.Item className="px-3 mt-2">
+            <Nav.Item className="px-3 mt-3">
                 {/* <Nav.Link href="/home">Active</Nav.Link> */}
                 <Link to="/about">About</Link>
             </Nav.Item>
-            <Nav.Item className="px-3 mt-2">
+            <Nav.Item className="px-3 mt-3">
                 <Login />
             </Nav.Item>
-            <Nav.Item className="px-3 mt-1">
+            <Nav.Item className="px-3 mt-2">
                 <Link className="btn btn-primary rounded-6" to="/dashboard">Get Started</Link>
             </Nav.Item>
             </Nav>
