08529646358b42f81464e26b388acef0b8d8fe26
Lane Garner <b.lane.garner@gmail.com>
2021-03-24 17:40:39 -0500

    about component

--- draftrr-react/src/components/About/About.jsx
+++ draftrr-react/src/components/About/About.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { ProfilePic } from "./ProfilePic"
+
+import "./About.scss"
 
 export const About = () => {
     return (
-        <div className="mg-1 body-container">
-            About
+        <div className="body-container container mg-1">
+            <section>
+                <h1 className="text-center">About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, earum sit! Vero totam quod minima aperiam repellendus a debitis ex tenetur suscipit perspiciatis quos odio, non id, esse illo aliquam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, earum sit! Vero totam quod minima aperiam repellendus a debitis ex tenetur suscipit perspiciatis quos odio, non id, esse illo aliquam.</p>
+            </section>
+            <section className="d-flex flex-column align-items-center my-5">
+                <h2 className="mb-5">Team</h2>
+                {/* <p className="mb-5">Click on each team member to learn more</p>   */}
+                <div className="profile-pics">
+                    <ProfilePic name="Nick Black" avatar="avatar" github="nblack0917" linkedin="nick-a-black" />
+                    <ProfilePic name="Chris Foy" avatar="avatar" github="rockman4417" linkedin="chris-foy123" />
+                    <ProfilePic name="Lane Garner" avatar="avatar" github="lanegarner" linkedin="lanegarner" />
+                    <ProfilePic name="Darryl Schomberg ii" avatar="avatar" github="dschombergii" linkedin="darrylschombergii" />
+                </div>
+            </section>
         </div>
     )
 }
