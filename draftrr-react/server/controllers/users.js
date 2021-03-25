c6a29e4e210964931c5e8543bf93c77480e8d604
dschombergii <d.schombergii@gmail.com>
2021-03-25 16:02:39 -0500

    fixed add user query

--- draftrr-react/server/controllers/users.js
+++ draftrr-react/server/controllers/users.js
@@ -6,9 +6,9 @@ const { handleSQLError } = require('../sql/error')
 
 const createUser = (req, res) => {
 
-  let sql = "INSERT INTO Users (Username, Email, Password, Theme, UserTimeframe, UserMaxCharacters, UserFont, NewUser) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
+  let sql = "INSERT INTO Users (ID, Username, Email, Password, Theme, UserTimeframe, UserMaxCharacters, UserFont, NewUser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"
 
-  sql = mysql.format(sql, [req.body.username, req.body.email, req.body.password, req.body.theme, req.body.userTimeframe, req.body.userMaxCharacters, req.body.userFont, req.body.newUser])
+  sql = mysql.format(sql, [req.body.id, req.body.username, req.body.email, req.body.password, req.body.theme, req.body.userTimeframe, req.body.userMaxCharacters, req.body.userFont, req.body.newUser])
 
   console.log("hit create user", sql)
 
@@ -37,10 +37,10 @@ const getUserByID = (req, res) => {
   console.log('sql', sql)
   console.log("querying user by id", req.params.id)
   pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err) 
+    if (err) return handleSQLError(res, err)
     console.log(res)
     return res.json(rows);
-  } 
+  }
   )
 }
 
