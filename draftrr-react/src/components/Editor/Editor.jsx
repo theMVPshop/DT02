97659deed23b0dcc0bca4ce96faae30c752fd7af
Lane Garner <b.lane.garner@gmail.com>
2021-03-26 15:29:14 -0500

    basic editor component

--- draftrr-react/src/components/Editor/Editor.jsx
+++ draftrr-react/src/components/Editor/Editor.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useRef } from "react"
+
 import "./Editor.scss"
 
 export const Editor = () => {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [inputRef]);
     return (
-        <div className="body-container">
-            Editor
+        <div className="body-container editor-container p-5">
+            <div className="d-flex justify-content-between align-items-end">
+                <button>Draft Settings</button>
+                <div className="d-flex flex-column align-items-center">
+                    <label htmlFor="draftTitle">Draft Title</label>
+                    <input id="draftTitle" type="text"/>
+                </div>
+                <div>
+                    <button className="mr-2">Save For Later</button>
+                    <button>Submit</button>
+                </div>
+
+            </div>
+            <div name="mainTextBox" id="mainTextBox" ref={inputRef} contenteditable="true">
+                <span id="locked"></span>
+            </div>
         </div>
     )
 }
--- draftrr-react/src/components/Footer/Footer.jsx
+++ draftrr-react/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     return (
         <footer className="d-flex justify-content-between align-items-center">
             <div>© {new Date().getFullYear()} Draftrr</div>
-            <a href="https://github.com" target="_blank" className="text-dark">
+            <a href="https://github.com/theMVPshop/DT02/tree/main/draftrr-react" target="_blank" className="text-dark">
                 <FaGithub style={{fontSize: "2.8em"}} />
             </a>
         </footer>
