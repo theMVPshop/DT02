40a731f20302160ee0875c51a5e1a72a69bf80dc
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2021-03-26 14:51:33 -0500

    worked on login page

--- draftrr-react/src/components/Login/Login.jsx
+++ draftrr-react/src/components/Login/Login.jsx
@@ -1,16 +1,32 @@
+import {useState} from 'react'
 import netlifyIdentity from "netlify-identity-widget"
+import axios from 'axios'
 
 netlifyIdentity.init()
 
 export const Login = () => {
 
+    const [userID, setUserID] = useState()
+
+    const getUserByID = (id) => {
+        axios.get(`http://localhost:4000/users/${id}`).then( res => setUserID({data : res}))
+
+        axios.post(`http://localhost:4000/users/`).then( res => setUserID({data : res}))
+        
+        console.log('user ID', userID)
+
+
+    }
+
+
+
     const handleClick = () => {
         netlifyIdentity.open()
         
         netlifyIdentity.on("login", user => {
-            console.log('user', user)
-            console.log("email: ", user.email)
-            console.log("name: ", user.user_metadata.full_name)
+            console.log('user', user.id)
+            getUserByID(user.id)
+            
         })
     }
 
