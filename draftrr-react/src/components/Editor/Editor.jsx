bc46e8a618f4c2270284da729fcf6768190cfe6b
Lane Garner <b.lane.garner@gmail.com>
2021-03-26 16:03:23 -0500

    friday push

--- draftrr-react/src/components/Editor/Editor.jsx
+++ draftrr-react/src/components/Editor/Editor.jsx
@@ -1,13 +1,35 @@
-import { useEffect, useRef } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import "./Editor.scss"
 
 export const Editor = () => {
-    const inputRef = useRef(null);
+    const [value, setValue] = useState("")
+    const [letters, setLetters ] = useState([])
+    const [charLimit, setCharLimit] = useState(3)
+    
+    useEffect(()=> {
+        let newValue
+        if (value.length > 0) {
+        newValue = value.split('')
+        setLetters(newValue)
+        }
+    }, [value])
+    
+    useEffect(()=> {
+        if (letters.length > charLimit) {
+        const newLetters = letters
+        newLetters.shift()
+        setLetters(newLetters)
+        setValue(newLetters.join(""))
+        }
+    }, [letters])
+
+    const inputRef = useRef(null)
 
     useEffect(() => {
-        inputRef.current.focus();
-    }, [inputRef]);
+        inputRef.current.focus()
+    }, [inputRef])
+
     return (
         <div className="body-container editor-container p-5">
             <div className="d-flex justify-content-between align-items-end">
@@ -20,11 +42,13 @@ export const Editor = () => {
                     <button className="mr-2">Save For Later</button>
                     <button>Submit</button>
                 </div>
-
             </div>
-            <div name="mainTextBox" id="mainTextBox" ref={inputRef} contenteditable="true">
-                <span id="locked"></span>
+            <div name="mainTextBox" id="mainTextBox" dangerouslySetInnerHTML={value.value} onChange={(e)=>setValue(e.target.value)} ref={inputRef} contentEditable="true">
+                {/* <span id="locked"></span> */}
             </div>
+            <textarea value={value} onChange={(e)=>setValue(e.target.value)}>
+                <span></span>
+            </textarea>
         </div>
     )
 }
