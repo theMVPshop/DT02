8346e8562677932a4266ae8db9eafacc10b0e717
Lane Garner <b.lane.garner@gmail.com>
2021-03-31 19:20:44 -0500

    fixed some styles that were hitting other components in the about page

--- draftrr-react/src/components/About/ProfilePic.jsx
+++ draftrr-react/src/components/About/ProfilePic.jsx
@@ -7,8 +7,8 @@ export const ProfilePic = ({name, role, avatar, github, linkedin}) => {
             <h3>{name}</h3>
             <h5>{role}</h5>
             <Avatar devName={avatar} width='200' className="circle" />
-            <a href={`https://github.com/${github}`} target="_blank" rel="noreferrer">GitHub</a>
-            <a href={`https://linkedin.com/${linkedin}`} target="_blank" rel="noreferrer">LinkedIn</a>
+            <a className="about-link" href={`https://github.com/${github}`} target="_blank" rel="noreferrer">GitHub</a>
+            <a className="about-link" href={`https://linkedin.com/${linkedin}`} target="_blank" rel="noreferrer">LinkedIn</a>
         </div>
     )
 }
