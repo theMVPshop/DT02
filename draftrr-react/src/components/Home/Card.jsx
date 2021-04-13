f3e2b46af6106fb42f0c78473eea42ad88d56e58
Keith Kritselis <kkritselis@gmail.com>
2021-04-13 12:19:58 -0500

    edited front page cards

--- draftrr-react/src/components/Home/Card.jsx
+++ draftrr-react/src/components/Home/Card.jsx
@@ -4,7 +4,7 @@ export const Card = ({card}) => {
     if (card === 'card1') {
         return (
             <div className="home-card d-flex flex-column align-items-center">
-                <h3 className="card-heading">Card 1</h3>
+                <h3 className="card-heading">Zero Distraction</h3>
                 <div className="w-80 p-2">
                     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In ornare quam viverra orci sagittis eu volutpat.</p>
                 </div>
@@ -13,7 +13,7 @@ export const Card = ({card}) => {
     } else if (card === 'card2') {
         return (
             <div className="home-card d-flex flex-column align-items-center">
-            <h3 className="card-heading">Card 2</h3>
+            <h3 className="card-heading">Minimal Editing</h3>
             <div className="w-80 p-2">
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat interdum varius sit amet mattis vulputate enim.</p>
             </div>
@@ -22,7 +22,7 @@ export const Card = ({card}) => {
     } else if (card === 'card3') {
         return (
             <div className="home-card d-flex flex-column align-items-center">
-            <h3 className="card-heading">Card 3</h3>
+            <h3 className="card-heading">100% Accountability</h3>
             <div className="w-80 p-2">
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque sagittis purus sit amet volutpat consequat mauris nunc congue.</p>
             </div>
